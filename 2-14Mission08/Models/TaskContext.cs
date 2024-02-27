9c1137baf4329521848fc9bc5cc5ec5b32db015d
brysonbarney <brysonbarney@BRYSONBARNEBD95>
2024-02-27 15:50:59 -0700

    models

---
+++ 2-14Mission08/Models/TaskContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace _2_14Mission08.Models;
+
+public partial class TaskContext : DbContext
+{
+    public TaskContext()
+    {
+    }
+
+    public TaskContext(DbContextOptions<TaskContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<TaskList> TaskLists { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=task.sqlite");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.CategoryId)
+                .ValueGeneratedNever()
+                .HasColumnName("CategoryID");
+        });
+
+        modelBuilder.Entity<TaskList>(entity =>
+        {
+            entity.HasKey(e => e.TaskId);
+
+            entity.ToTable("TaskList");
+
+            entity.Property(e => e.TaskId).HasColumnName("TaskID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.DueDate).HasColumnName("Due Date");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.TaskLists).HasForeignKey(d => d.CategoryId);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ 2-14Mission08/Models/TaskList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace _2_14Mission08.Models;
+
+public partial class TaskList
+{
+    public int TaskId { get; set; }
+
+    [Required]
+    public string Task { get; set; } = null!;
+
+    public string? DueDate { get; set; }
+
+    [Required]
+    public int Quadrant { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public int? Completed { get; set; }
+
+    public virtual Category? Category { get; set; }
+}
