61ce39fd5ba2fcba2bafc8f6114a2cb7dfa3c970
adamtrommlitz <adamtrommlitz3@gmail.com>
2024-02-26 18:47:20 -0700

    my best changes

--- 2-14Mission08/Controllers/HomeController.cs
+++ 2-14Mission08/Controllers/HomeController.cs
@@ -4,29 +4,87 @@ using System.Diagnostics;
 
 namespace _2_14Mission08.Controllers
 {
-    public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        //Use the home controller to to navigate through the views and use the move collection to post to the database
+        public class HomeController : Controller
         {
-            _logger = logger;
-        }
+            private //Need context file here _context;
 
-        public IActionResult Index()
+            //Set up the views for our website to use
+
+            public HomeController(MoviesCollectionContext temp) //Constructor
+            {
+                _context = temp;
+            }
+
+
+
+            public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult MatrixForm()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpGet]
+        public IActionResult Form()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("MoviesCollection", new MovieCollection());
+
         }
+
+        [HttpPost]
+        public IActionResult MatrixForm(//Modelname response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Tasks.Add(response); // Add record to the database
+                _context.SaveChanges();
+                return View("Index", response);
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories.ToList();
+
+                return View(response);
+            }
+        }
+
+        public IActionResult MovieTable()
+        {
+            var tasks = _context.Tasks.ToList();
+
+            return View(tasks);
+
+        }
+
+        //get the movie id from the database and allow changes to be made
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordtoEdit = _context.Tasks
+                .Single(x => x.TaskId == id);
+
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("MoviesCollection", recordtoEdit);
+
+        }
+
+        //Update and save the changes to the table 
+        [HttpPost]
+        public IActionResult Edit(MovieCollection updatedinfo)
+        {
+            _context.Update(updatedinfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("MovieTable");
+        }
+
+
     }
 }
